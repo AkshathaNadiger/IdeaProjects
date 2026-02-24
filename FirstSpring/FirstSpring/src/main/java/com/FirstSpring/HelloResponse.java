0c9a850845889feaabe5262dce9731cc5911ae90
AkshathaNadiger <akshathanadiger20gamil.com>
2026-02-24 21:57:47 +0530

    all

---
+++ FirstSpring/FirstSpring/src/main/java/com/FirstSpring/HelloResponse.java
@@ -0,0 +1,18 @@
+package com.FirstSpring;
+
+public class HelloResponse {
+
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
